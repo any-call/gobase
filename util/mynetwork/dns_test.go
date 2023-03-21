fe2a74404446ac1abd5a1cd7962b058490ef1061
guihuaJin <guihuaJin@rwsoftware.cn>
2023-03-21 17:27:02 +0800

    add json class

--- util/mynetwork/dns_test.go
+++ util/mynetwork/dns_test.go
@@ -1,6 +1,7 @@
 package mynetwork
 
 import (
+	"net"
 	"testing"
 	"time"
 )
@@ -8,4 +9,15 @@ import (
 func Test_DigNS(t *testing.T) {
 	ns := DigNS("v5dns.xyz", time.Second)
 	t.Log(ns)
+}
+
+func Test_TT(t *testing.T) {
+	dns := "110.242.68.66"
+	ns, err := net.LookupAddr(dns)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Log("ns:", ns)
 }
