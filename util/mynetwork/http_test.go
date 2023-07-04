281fcaf33b0c5856536e1ee1c6075dedb7aeb76e
jinguihua <156711203@qq.com>
2023-07-04 10:24:26 +0800

    change

--- util/mynetwork/http_test.go
+++ util/mynetwork/http_test.go
@@ -29,3 +29,26 @@ func Test_aaaa(t *testing.T) {
 
 	t.Log("ret is :", resp)
 }
+
+func BenchmarkGetJson(t *testing.B) {
+	var resp map[string]any
+	err := GetJson("http://localhost:8081/api/site/sync", nil, 0, func(ret []byte, httpCode int) error {
+		if httpCode != http.StatusOK {
+			return fmt.Errorf("%d with %s", httpCode, string(ret))
+		}
+
+		err := json.Unmarshal(ret, &resp)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Log("ret is :", resp)
+}
--- util/mysql/base.go
+++ util/mysql/base.go
@@ -1,14 +1,16 @@
 package mysql
 
-import "fmt"
-
 const (
 	DateFormatter1 = "%Y-%m-%d %H:%i:%s"
 )
 
-func DateToStr(dateColumn string, format string) string {
-	return fmt.Sprintf("DATE_FORMAT(%s,'%s')", dateColumn, format)
-}
+//func DateToStr(field string, format string) string {
+//	return fmt.Sprintf("date_format(%s,'%s')", field, format)
+//}
+//
+//func StrToDate(dateStr string, format string) string {
+//	return fmt.Sprintf("str_to_date('%s','%s')", dateStr, format)
+//}
 
 //DATE_FORMAT(convert_tz(check_time,'UTC','Asia/Shanghai'),'%Y-%m-%d %H:%i:%s')  as last_check_time,
 //DATE_FORMAT(convert_tz(FROM_UNIXTIME(check_time),'Asia/Shanghai','Asia/Shanghai'),'%Y-%m-%d %H:%i:%s')  as last_check_time1,
