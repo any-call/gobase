5f19c6c26dbca893cf13d9f91e3457b2debe16bf
jinguihua <156711203@qq.com>
2024-08-01 09:40:11 +0800

    changed

--- util/mytime/base.go
+++ util/mytime/base.go
@@ -64,3 +64,11 @@ func EndOfDay(t time.Time) time.Time {
 	year, month, day := t.Date()
 	return time.Date(year, month, day, 23, 59, 59, 999999999, t.Location())
 }
+
+func CurrBeginningOfDay() time.Time {
+	return BeginningOfDay(time.Now())
+}
+
+func CurrEndOfDay() time.Time {
+	return EndOfDay(time.Now())
+}
