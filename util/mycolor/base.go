a7f05426c36c7f8b356785c222c56941a5a844b3
jinguihua <156711203@qq.com>
2024-08-16 17:03:59 +0800

    chagned

--- util/mycolor/base.go
+++ util/mycolor/base.go
@@ -4,13 +4,17 @@ import (
 	"image/color"
 )
 
-func UInt32ToRGBA(rgba uint32) color.Color {
+func RGBAUint(rgba uint32) color.Color {
 	r := uint8((rgba >> 24) & 0xFF) // 提取红色通道
 	g := uint8((rgba >> 16) & 0xFF) // 提取绿色通道
 	b := uint8((rgba >> 8) & 0xFF)  // 提取蓝色通道
 	a := uint8(rgba & 0xFF)         // 提取透明度通道
 
 	return color.RGBA{R: r, G: g, B: b, A: a}
+}
+
+func RGB(r, g, b uint8) color.Color {
+	return RGBA(r, g, b, 0xff)
 }
 
 func RGBA(r, g, b, a uint8) color.Color {
