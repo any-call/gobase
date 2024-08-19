b0d8cb612c10e7573bbd09d0db99809f6ec86c85
jinguihua <156711203@qq.com>
2024-08-19 16:42:03 +0800

    changed

--- util/myjson/time.go
+++ util/myjson/time.go
@@ -46,6 +46,9 @@ func (t *Time) UnmarshalJSON(data []byte) error {
 func (t *Time) MarshalJSON() ([]byte, error) {
 	return json.Marshal(time.Time(*t).Format("2006-01-02T15:04:05.000Z"))
 }
+func (t Time) ToTime() time.Time {
+	return time.Time(t)
+}
 
 type Date time.Time
 
@@ -66,6 +69,10 @@ func (d *Date) MarshalJSON() ([]byte, error) {
 	return json.Marshal(time.Time(*d).Format("2006-01-02"))
 }
 
+func (t Date) ToTime() time.Time {
+	return time.Time(t)
+}
+
 // Millis represents a Unix time in milliseconds since January 1, 1970 UTC.
 type Millis time.Time
 
@@ -80,6 +87,10 @@ func (m *Millis) UnmarshalJSON(data []byte) error {
 
 func (m Millis) MarshalJSON() ([]byte, error) {
 	return json.Marshal(time.Time(m).UnixMilli())
+}
+
+func (t Millis) ToTime() time.Time {
+	return time.Time(t)
 }
 
 // Nanos represents a Unix time in nanoseconds since January 1, 1970 UTC.
@@ -99,4 +110,8 @@ func (n *Nanos) UnmarshalJSON(data []byte) error {
 
 func (n Nanos) MarshalJSON() ([]byte, error) {
 	return json.Marshal(time.Time(n).UnixNano())
+}
+
+func (t Nanos) ToTime() time.Time {
+	return time.Time(t)
 }
