f76bceae2786758740ce0c0963b6c43d1224b6b3
jinguihua <156711203@qq.com>
2024-09-09 07:20:28 +0800

    changed

--- frame/myctrl/singleton.go
+++ frame/myctrl/singleton.go
@@ -2,17 +2,29 @@ package myctrl
 
 import "sync"
 
-// 单例对象
-// Singleton 是一个泛型的单例结构
-type Singleton[T any] struct {
-	instance *T
-	once     sync.Once
+// Singleton 是一个单例接口，包含 GetInstance 方法
+type Singleton[T any] interface {
+	GetInstance() *T
 }
 
-// GetInstance 获取单例对象的实例，只有在第一次调用时会创建新实例
-func (s *Singleton[T]) GetInstance(newInstance func() *T) *T {
+// NewSingleton 创建并返回一个实现 Singleton 接口的单例
+func NewSingleton[T any](newInstance func() *T) Singleton[T] {
+	return &singleton[T]{
+		newInstance: newInstance,
+	}
+}
+
+// singleton 是实现了 Singleton 接口的具体类型，不对外公开
+type singleton[T any] struct {
+	instance    *T
+	once        sync.Once
+	newInstance func() *T
+}
+
+// GetInstance 获取单例对象的实例
+func (s *singleton[T]) GetInstance() *T {
 	s.once.Do(func() {
-		s.instance = newInstance()
+		s.instance = s.newInstance()
 	})
 	return s.instance
 }
