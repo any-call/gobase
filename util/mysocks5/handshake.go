2ed9f01eaedc46b9c73e4e6fee92b2fda4cf931f
jinguihua <156711203@qq.com>
2024-10-25 17:52:04 +0800

    chagned

--- util/mysocks5/handshake.go
+++ util/mysocks5/handshake.go
@@ -1,6 +1,7 @@
 package mysocks5
 
 import (
+	"errors"
 	"fmt"
 	"github.com/any-call/gobase/frame/myctrl"
 	"github.com/any-call/gobase/util/mylog"
@@ -107,6 +108,116 @@ func Handshake(rw io.ReadWriter, authFn func(username, password string) bool) (A
 //+----+-----+-------+------+----------+----------+
 //| 1  |  1  | X'00' |  1   | Variable |    2     |
 //+----+-----+-------+------+----------+----------+
+
+func ConnToSocks5(addr Addr, remoteAddr string, authfn func() (userName, password string)) (net.Conn, error) {
+	conn, err := net.Dial("tcp", remoteAddr)
+	if err != nil {
+		return nil, fmt.Errorf("连接 SOCKS5 代理服务器失败:%v", err)
+	}
+
+	//建立链接
+	_, err = conn.Write(myctrl.ObjFun(func() []byte {
+		if authfn == nil {
+			return []byte{5, 1, 0}
+		}
+
+		return []byte{5, 2, 0, 2}
+	}))
+	if err != nil {
+		defer func() {
+			_ = conn.Close()
+		}()
+		return nil, fmt.Errorf("发送握手请求失败:%v", err)
+	}
+
+	//读服务端响应
+	response := make([]byte, 2)
+	_, err = conn.Read(response) // SOCKS request: VER, CMD, RSV, Addr
+	if err != nil {
+		defer func() {
+			_ = conn.Close()
+		}()
+		return nil, errors.New("读取握手响应失败")
+	}
+
+	if response[0] != 5 || (response[1] != 0 && response[1] != 2) {
+		defer func() {
+			_ = conn.Close()
+		}()
+		return nil, fmt.Errorf("SOCKS5 握手失败，代理服务器应答:%v", response)
+	}
+
+	if response[1] == 2 { //服务端需求用户名与密码认证
+		if authfn == nil {
+			defer func() {
+				_ = conn.Close()
+			}()
+			return nil, fmt.Errorf("SOCKS5 需求用户密码认证")
+		}
+		username, password := authfn()
+		mylog.Debug("用户名:", username, ";password:", password)
+		// 发送用户名/密码认证信息
+		auth := make([]byte, 3+len(username)+len(password))
+		auth[0] = 0x01                              // 认证版本
+		auth[1] = byte(len(username))               // 用户名长度
+		copy(auth[2:], username)                    // 用户名
+		auth[2+len(username)] = byte(len(password)) // 密码长度
+		copy(auth[3+len(username):], password)      // 密码
+
+		if _, err := conn.Write(auth); err != nil {
+			defer func() {
+				_ = conn.Close()
+			}()
+			return nil, fmt.Errorf("发送认证信息失败:%v", err)
+		}
+
+		// 读取认证结果
+		authResponse := make([]byte, 2)
+		if _, err := conn.Read(authResponse); err != nil {
+			defer func() {
+				_ = conn.Close()
+			}()
+			return nil, fmt.Errorf("读取认证响应失败:%v", err)
+		}
+
+		// 检查认证结果
+		if authResponse[1] != 0x00 {
+			defer func() {
+				_ = conn.Close()
+			}()
+			return nil, errors.New("用户名/密码认证失败")
+		}
+	}
+
+	buf := make([]byte, MaxReqLen)
+	//然后跟目标服务建立连接
+	copy(buf, []byte{5, 1, 0})
+	copy(buf[3:], addr)
+	_, err = conn.Write(buf[:3+len(addr)])
+	if err != nil {
+		defer func() {
+			_ = conn.Close()
+		}()
+		return nil, fmt.Errorf("SOCKS5 建立链接时出错：%v", err)
+	}
+
+	n, err := conn.Read(buf)
+	if err != nil {
+		defer func() {
+			_ = conn.Close()
+		}()
+		return nil, fmt.Errorf("SOCKS5 读取出错:%v", err)
+	}
+
+	if buf[0] != 5 || buf[1] != 0 {
+		defer func() {
+			_ = conn.Close()
+		}()
+		return nil, fmt.Errorf("SOCKS5 响应错误：%v", buf[:n])
+	}
+
+	return conn, nil
+}
 
 // 处理客户端认证请求
 func authenticate(rw io.ReadWriter, validfn func(username, password string) bool) bool {
